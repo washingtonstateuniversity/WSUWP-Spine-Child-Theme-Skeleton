febaf32214eac84fb976972120993daa63bf0643
jeremyfelt <jeremy.felt@gmail.com>
2014-04-22 15:47:20 -0700

    Add a sample functions.php file

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Custom functionality required by your child theme can go here. Use this
+ * to override any defaults provided by the Spine parent theme through
+ * the provided actions and filters.
+ */
