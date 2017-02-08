10f0b61fc476de5a68540fb7cfe1a6424c414bf1
Jeremy Felt <jeremy.felt@gmail.com>
2017-02-08 10:01:53 -0800

    Add a JSCS and jshint configuration
    By default this will really only check the Gruntfile.js, but it
    leaves an easy framework for adding your own JS files to the theme.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,62 +1,108 @@
-module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+module.exports = function( grunt ) {
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON( "package.json" ),
 
-        stylelint: {
-            src: [ "css/*.css" ]
-        },
+		stylelint: {
+			src: [ "css/*.css" ]
+		},
 
-        concat: {
-            options: {
-                sourceMap: true
-            },
-            dist: {
-                src: 'css/*.css',
-                dest: 'tmp-style.css'
-            }
-        },
+		concat: {
+			options: {
+				sourceMap: true
+			},
+			dist: {
+				src: "css/*.css",
+				dest: "tmp-style.css"
+			}
+		},
 
-        postcss: {
-            options: {
-                map: true,
-                diff: false,
-                processors: [
-                    require( "autoprefixer" )( {
-                        browsers: [ "> 1%", "ie 8-11", "Firefox ESR" ]
-                    } )
-                ]
-            },
-            dist: {
-                src: "tmp-style.css",
-                dest: "style.css"
-            }
-        },
+		postcss: {
+			options: {
+				map: true,
+				diff: false,
+				processors: [
+					require( "autoprefixer" )( {
+						browsers: [ "> 1%", "ie 8-11", "Firefox ESR" ]
+					} )
+				]
+			},
+			dist: {
+				src: "tmp-style.css",
+				dest: "style.css"
+			}
+		},
 
-        clean: {
-            options: {
-                force: true
-            },
-            temp: [ 'tmp-style.css', 'tmp-style.css.map' ]
-        },
+		clean: {
+			options: {
+				force: true
+			},
+			temp: [ "tmp-style.css", "tmp-style.css.map" ]
+		},
 
-        phpcs: {
-            plugin: {
-                src: './'
-            },
-            options: {
-                bin: "vendor/bin/phpcs --extensions=php --ignore=\"*/vendor/*,*/node_modules/*\"",
-                standard: "phpcs.ruleset.xml"
-            }
-        }
+		jscs: {
+			scripts: {
+				src: [ "Gruntfile.js", "src/js/*.js" ],
+				options: {
+					preset: "jquery",
+					requireCamelCaseOrUpperCaseIdentifiers: false, // We rely on name_name too much to change them all.
+					maximumLineLength: 250,
+					validateIndentation: "\t"
+				}
+			}
+		},
 
-    });
+		jshint: {
+			grunt_script: {
+				src: [ "Gruntfile.js" ],
+				options: {
+					curly: true,
+					eqeqeq: true,
+					noarg: true,
+					quotmark: "double",
+					undef: true,
+					unused: false,
+					node: true     // Define globals available when running in Node.
+				}
+			},
+			theme_scripts: {
+				src: [ "src/js/*.js" ],
+				options: {
+					bitwise: true,
+					curly: true,
+					eqeqeq: true,
+					forin: true,
+					freeze: true,
+					noarg: true,
+					nonbsp: true,
+					quotmark: "double",
+					undef: true,
+					unused: true,
+					browser: true, // Define globals exposed by modern browsers.
+					jquery: true   // Define globals exposed by jQuery.
+				}
+			}
+		},
 
-    grunt.loadNpmTasks( "grunt-postcss" );
-    grunt.loadNpmTasks( "grunt-contrib-concat" );
-    grunt.loadNpmTasks( "grunt-contrib-clean" );
-    grunt.loadNpmTasks( "grunt-phpcs" );
-    grunt.loadNpmTasks( "grunt-stylelint" );
+		phpcs: {
+			plugin: {
+				src: "./"
+			},
+			options: {
+				bin: "vendor/bin/phpcs --extensions=php --ignore=\"*/vendor/*,*/node_modules/*\"",
+				standard: "phpcs.ruleset.xml"
+			}
+		}
 
-    // Default task(s).
-    grunt.registerTask('default', ['stylelint', 'concat', 'postcss', 'clean']);
+	} );
+
+	grunt.loadNpmTasks( "grunt-postcss" );
+	grunt.loadNpmTasks( "grunt-contrib-concat" );
+	grunt.loadNpmTasks( "grunt-contrib-clean" );
+	grunt.loadNpmTasks( "grunt-phpcs" );
+	grunt.loadNpmTasks( "grunt-stylelint" );
+	grunt.loadNpmTasks( "grunt-jscs" );
+	grunt.loadNpmTasks( "grunt-contrib-jshint" );
+
+	// Default task(s).
+	grunt.registerTask( "default", [ "jscs", "jshint", "stylelint", "concat", "postcss", "clean" ] );
 };
