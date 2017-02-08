91e734493e3031da0484d75e81011a36bdbf1aa8
Jeremy Felt <jeremy.felt@gmail.com>
2017-02-08 09:51:08 -0800

    Update style lint configuration
    * Several stylelint rules were deprecated upstream
    * CSS Lint is unnecessary now that we use stylelint
    * Update NPM dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,41 +32,6 @@ module.exports = function(grunt) {
             }
         },
 
-        csslint: {
-            main: {
-                src: [ "style.css" ],
-                options: {
-                    "fallback-colors": false,              // unless we want to support IE8
-                    "box-sizing": false,                   // unless we want to support IE7
-                    "compatible-vendor-prefixes": false,   // The library on this is older than autoprefixer.
-                    "gradients": false,                    // This also applies ^
-                    "overqualified-elements": false,       // We have weird uses that will always generate warnings.
-                    "ids": false,
-                    "regex-selectors": false,              // audit
-                    "adjoining-classes": false,
-                    "box-model": false,                    // audit
-                    "universal-selector": false,           // audit
-                    "unique-headings": false,              // audit
-                    "outline-none": false,                 // audit
-                    "floats": false,
-                    "font-sizes": false,                   // audit
-                    "important": false,                    // This should be set to 2 one day.
-                    "unqualified-attributes": false,       // Should probably be 2 one day.
-                    "qualified-headings": false,
-                    "known-properties": 1,              // Okay to ignore in the case of known unknowns.
-                    "duplicate-background-images": 2,
-                    "duplicate-properties": 2,
-                    "star-property-hack": 2,
-                    "text-indent": 2,
-                    "display-property-grouping": 2,
-                    "shorthand": 2,
-                    "empty-rules": 2,
-                    "vendor-prefix": 2,
-                    "zero-units": 2
-                }
-            }
-        },
-
         clean: {
             options: {
                 force: true
@@ -88,11 +53,10 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks( "grunt-postcss" );
     grunt.loadNpmTasks( "grunt-contrib-concat" );
-    grunt.loadNpmTasks( "grunt-contrib-csslint" );
     grunt.loadNpmTasks( "grunt-contrib-clean" );
     grunt.loadNpmTasks( "grunt-phpcs" );
     grunt.loadNpmTasks( "grunt-stylelint" );
 
     // Default task(s).
-    grunt.registerTask('default', ['stylelint','concat', 'postcss', 'csslint', 'clean']);
+    grunt.registerTask('default', ['stylelint', 'concat', 'postcss', 'clean']);
 };
